d496a885bbf71b8967c5ff588f5fa4352456f6d0
Moosabilal <moosabilal75608@gmail.com>
2026-01-01 02:34:28 +0530

    updated vite config file removed proxy

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: true, 
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-        // The Magic Trick: Force the Origin header to match what the backend expects
-        configure: (proxy) => {
-          proxy.on('proxyReq', (proxyReq) => {
-            proxyReq.setHeader('Origin', 'http://localhost:5173');
-          });
-        }
-      },
-    },
-  },
 })

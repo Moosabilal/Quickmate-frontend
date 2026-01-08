83c3bedf21cca2d424498528a399cb9ff2add5a3
Moosabilal <moosabilal75608@gmail.com>
2026-01-08 21:48:43 +0530

    updated reset password page

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 
 const config = {
 // 'http://localhost:5000
-  API_BASE_URL: 'http://localhost:5000/api',
+  API_BASE_URL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api",
 };
 
 export default config;
